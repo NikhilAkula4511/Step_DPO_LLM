c0f6dbc2dfaa8a2321d9c2b68001b7f3f48cc4ac
NikhilAkula4511 <105285055+NikhilAkula4511@users.noreply.github.com>
2025-07-17 20:25:37 +0530

    Update test_reward_model.py

--- scripts/test_reward_model.py
+++ scripts/test_reward_model.py
@@ -5,8 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from models.reward_model import StepwiseRewardModel
 
 
-api_key = "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"  # Replace with your OpenAI API key
-  # Make sure it's exported
+api_key = api_key# Make sure it's exported
 model = StepwiseRewardModel(api_key)
 
 prompt = "Why do humans need sleep?"
